f210b543eb171d74be29342e4c46ee4977a60bf9
SirGregory <124635852+SirGreg0ry@users.noreply.github.com>
2024-03-30 19:07:01 -0600

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-  1 #!/bin/bash 
-  2 
-  3 sudo apt-get update && sudo apt-get upgrade
-  4 sudo apt update
-  5 sudo apt install update
-  6 sudo apt full-upgrade
-  7 sudo apt install upgrade
-  8 sudo apt autoremove
-  9 
